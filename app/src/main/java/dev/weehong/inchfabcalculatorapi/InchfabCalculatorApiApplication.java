c65e3aeac784c5090b1ce122d43a84f30732d303
Wee Hong KOH <weehongkoh@Wee-Hongs-16-inch-MacBook-Pro.local>
2024-02-16 13:26:44 +0800

    add submodule

---
+++ app/src/main/java/dev/weehong/inchfabcalculatorapi/InchfabCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package dev.weehong.inchfabcalculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchfabCalculatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InchfabCalculatorApiApplication.class, args);
+    }
+
+}
---
+++ app/src/test/java/dev/weehong/inchfabcalculatorapi/InchfabCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.weehong.inchfabcalculatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InchfabCalculatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
